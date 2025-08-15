605e6c8bd23e77395e65ffc5ea6124bf84a1261e
smetanin-e <e91smet15@gmail.com>
2025-08-15 21:10:08 +0300

    создание api для получения книг по slug

--- src/lib/send-email.ts
+++ src/lib/send-email.ts
@@ -3,7 +3,7 @@ import { Resend } from 'resend';
 export const sendEmail = async (to: string,subject: string, template: React.ReactNode) => {
     const resend = new Resend(process.env.RESEND_API_KEY);
     const { data, error } = await resend.emails.send({
-    from: 'Acme <onboarding@resend.dev>',
+    from: 'Books online <onboarding@resend.dev>',
     to,
     subject,
     react: template,
